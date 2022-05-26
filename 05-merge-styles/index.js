ae4cba40090ccfa065b739e217803f301c3106e1
Clukva <72574305+Clukva@users.noreply.github.com>
2022-05-26 21:13:57 +0300

    fix: bag

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -15,16 +15,16 @@ function readdirCallBack(error, files){
 
 
            files.forEach(elem =>{
-            fs.stat((pathP+'\\'+`${elem}`), (error, stats) => {
+            fs.stat((pathP+'\/'+`${elem}`), (error, stats) => {
                 if(error) throw error;
                 if(!stats.isFile() || path.extname(elem) !== '.css') return;
               
             
-              fs.readFile(pathP+'\\'+`${elem}`, 'utf-8', (error, data)=>{
+              fs.readFile(pathP+'\/'+`${elem}`, 'utf-8', (error, data)=>{
                   if(error){
                       throw error;
                   } else{
-                    fs.appendFile(pathPath+ '\\bundle.css', data, function (err) {
+                    fs.appendFile(pathPath+ '\/bundle.css', data, function (err) {
                         if (err) throw err;
                        // console.log('Saved!');
                      });
@@ -35,7 +35,7 @@ function readdirCallBack(error, files){
     }
   }
 function fileHandler(){
-    fs.open( pathPath + '\\'+'bundle.css', 'w', (err) => {
+    fs.open( pathPath + '\/'+'bundle.css', 'w', (err) => {
         if(err){ 
             throw err;
         }
@@ -43,7 +43,7 @@ function fileHandler(){
 }
 
 function deleteBundle(){
-    fs.unlink(  pathPath + '\\'+'bundle.css', err =>{
+    fs.unlink(  pathPath + '\/'+'bundle.css', err =>{
             if(err) console.log('add file');
       })
 }
